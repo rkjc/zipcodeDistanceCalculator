380671164f4d6df0247e68752eba7172f5f7b2cf
Richard Cross <richardkjcross@gmail.com>
2013-05-10 21:09:19 -0700

    from laptop

--- src/ZipMain.java
+++ src/ZipMain.java
@@ -7,6 +7,8 @@ public class ZipMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//double dist = ZipDistCalc.calcDistance(zip1, zip2);
+		//System.out.println("distance is " + dist);
 	}
 
 }
