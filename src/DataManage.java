5503798a609eb5a7371df89d850ec396ee29acba
rkjc <richardkjcross@gmail.com>
2013-05-13 01:14:19 -0700

    all functions working - needs to be cleaned up

--- src/DataManage.java
+++ src/DataManage.java
@@ -3,46 +3,53 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
 
 public class DataManage {
 
-	public static void makeZipCollection(){
-		//parses zipcode list and loads it to the collection zipLoc	
+	public static void makeZipCollection() {
+		// parses zipcode list and loads it to the collection zipLoc
 	}
-	
-	public static void testForZipsCsv(){
+
+	public static void testForZipsCsv() {
 		File file = new File("zips.csv");
 		boolean test = file.isFile();
 		System.out.println("zips.csv exists = " + test);
 	}
-	
-	public static void parseZipsCsv(){
+
+	public static void parseZipsCsv() {
 		File inFile = new File("zips.csv");
 		File outFile = new File("zipLocs.csv");
-		
-		if(inFile.isFile()){
+
+		if (inFile.isFile()) {
 			try {
 				BufferedReader br = new BufferedReader(new FileReader(inFile));
 				BufferedWriter bw = new BufferedWriter(new FileWriter(outFile));
-				
-				
-				String line = br.readLine(); //first one gets rid of the header
+
+				String line = br.readLine(); // first one gets rid of the header
 				line = br.readLine();
-	
+
 				String zip = "";
 				String lon = "";
 				String lat = "";
-				
-				while (line != null) {	
-				//for(int i=0; i < 65; i++){	
+
+				while (line != null) {
+					// for(int i=0; i < 65; i++){
 					zip = line.substring(1, 6);
 					lat = line.substring(17, 26);
 					lon = line.substring(30, 40);
-					
-					if(isInteger(zip)){
-						StringBuilder sb = new StringBuilder();	
+
+					if (isInteger(zip)) {
+						StringBuilder sb = new StringBuilder();
 						sb.append(zip);
 						sb.append(",");
 						sb.append(lat);
@@ -51,27 +58,27 @@ public class DataManage {
 						sb.append("\n");
 						bw.append(sb);
 					}
-					
+
 					line = br.readLine();
 				}
-							
+
 				br.close();
 				bw.close();
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
 		}
-		
+
 	}
-	
-	public static String readFileInput(File file){
-		String everything = "";		
-		if(file.isFile()){
+
+	public static String readFileInput(File file) {
+		String everything = "";
+		if (file.isFile()) {
 			try {
 				BufferedReader br = new BufferedReader(new FileReader(file));
 				StringBuilder sb = new StringBuilder();
 				String line = br.readLine();
-	
+
 				while (line != null) {
 					sb.append(line);
 					sb.append("\n");
@@ -79,23 +86,183 @@ public class DataManage {
 				}
 				everything = sb.toString();
 				br.close();
-	
+
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
-		}
-		else {
+		} else {
 			System.out.println("could not find file");
 		}
 		return everything;
 	}
-	
+
 	public static boolean isInteger(String s) {
-	    try { 
-	        Integer.parseInt(s); 
-	    } catch(NumberFormatException e) { 
-	        return false; 
-	    }	    
-	    return true; // only got here if we didn't return false
+		try {
+			Integer.parseInt(s);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true; // only got here if we didn't return false
 	}
+
+	public static void printSelectZipcodes() throws Exception {
+
+		System.out.println("printing select zipcodes");
+
+		MongoClient mongoClient = new MongoClient("localhost");
+
+		DB db = mongoClient.getDB("ziptest");
+
+		DBCollection coll = db.getCollection("zipcodeLocations");
+
+		DBCursor cursor;
+
+		// gets lat and lon for zipcode thisZip
+		Integer thisZip = 90032;
+		Integer zip = 0;
+		Double lat = 0.0;
+		Double lon = 0.0;
+		BasicDBObject query = new BasicDBObject("zipcode", thisZip);
+		cursor = coll.find(query);
+		try {
+			while (cursor.hasNext()) {
+				BasicDBObject obj = (BasicDBObject) cursor.next();
+				zip = obj.getInt("zipcode");
+				lat = obj.getDouble("latitude");
+				lon = obj.getDouble("longitude");
+				// result.add(obj.getString("zipcode"));
+				// System.out.println(cursor.next());
+			}
+		} finally {
+			cursor.close();
+		}
+
+		System.out.println(zip);
+		System.out.println(lat);
+		System.out.println(lon);
+		System.out.println("");
+
+		// print out the locations within distance collection
+		HashMap<Integer, Double> zipDist = new HashMap<Integer, Double>();
+		cursor = coll.find();
+
+		BasicDBObject obj;
+		Double distance = 0.0;
+
+		try {
+			System.out.println("zipcodes within 5 miles");
+			while (cursor.hasNext()) {
+				obj = (BasicDBObject) cursor.next();
+				distance = ZipDistCalc.calcDistance(lat, lon,
+						obj.getDouble("latitude"), obj.getDouble("longitude"));
+				if (distance <= 5) {
+					// System.out.println(cursor.next());
+					zipDist.put(obj.getInt("zipcode"), distance);
+				}
+			}
+			System.out.println("number of zipcodes = " + zipDist.size());
+			System.out.println(zipDist);
+
+			zipDist = new HashMap<Integer, Double>();
+			System.out.println("\nzipcodes between 5 and 10 miles");
+			cursor = coll.find();
+			while (cursor.hasNext()) {
+				obj = (BasicDBObject) cursor.next();
+				distance = ZipDistCalc.calcDistance(lat, lon,
+						obj.getDouble("latitude"), obj.getDouble("longitude"));
+				if (distance > 5 && distance <= 10) {
+					// System.out.println(cursor.next());
+					zipDist.put(obj.getInt("zipcode"), distance);
+				}
+			}
+			System.out.println("number of zipcodes = " + zipDist.size());
+			System.out.println(zipDist);
+
+			zipDist = new HashMap<Integer, Double>();
+			System.out.println("\nzipcodes between 10 and 20 miles");
+			cursor = coll.find();
+			while (cursor.hasNext()) {
+				obj = (BasicDBObject) cursor.next();
+				distance = ZipDistCalc.calcDistance(lat, lon,
+						obj.getDouble("latitude"), obj.getDouble("longitude"));
+				if (distance > 10 && distance <= 20) {
+					// System.out.println(cursor.next());
+					zipDist.put(obj.getInt("zipcode"), distance);
+				}
+			}
+			System.out.println("number of zipcodes = " + zipDist.size());
+			System.out.println(zipDist);
+			
+			zipDist = new HashMap<Integer, Double>();
+			System.out.println("\nzipcodes between 20 and 30 miles");
+			cursor = coll.find();
+			while (cursor.hasNext()) {
+				obj = (BasicDBObject) cursor.next();
+				distance = ZipDistCalc.calcDistance(lat, lon,
+						obj.getDouble("latitude"), obj.getDouble("longitude"));
+				if (distance > 20 && distance <= 30) {
+					// System.out.println(cursor.next());
+					zipDist.put(obj.getInt("zipcode"), distance);
+				}
+			}
+			System.out.println("number of zipcodes = " + zipDist.size());
+			System.out.println(zipDist);
+
+		} finally {
+			cursor.close();
+		}
+
+	}
+
+	public static void loadZipFileIntoDB() throws Exception {
+		System.out.println("loading zipdata to DB");
+
+		MongoClient mongoClient = new MongoClient("localhost");
+
+		DB db = mongoClient.getDB("ziptest");
+
+		DBCollection coll = db.getCollection("zipcodeLocations");
+
+		Integer zip = 0;
+		Double lat = 0.0;
+		Double lon = 0.0;
+
+		// load the zipcode lat-lon table into mongodb database ziptest
+		File inFile = new File("zipLocs.csv");
+		if (inFile.isFile()) {
+			coll.drop();
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(inFile));
+				String line = br.readLine();
+
+				while (line != null) {
+					// for(int i = 0; i < 1000; i++){
+					zip = Integer.valueOf(line.substring(0, 5));
+					lat = Double.valueOf(line.substring(6, 15));
+					lon = Double.valueOf(line.substring(16, 26).trim());
+
+					BasicDBObject doc = new BasicDBObject("zipcode", zip)
+							.append("latitude", lat).append("longitude", lon);
+
+					coll.insert(doc);
+					line = br.readLine();
+				}
+
+				br.close();
+
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		} else {
+			System.out.println("could not find file");
+		}
+
+	}
+	
+	// inserts a single document
+	// BasicDBObject doc = new BasicDBObject("zipcode", zip).
+	// append("latitude", lat).
+	// append("longitude", lon);
+	//
+	// coll.insert(doc);
 }
--- src/ZipMain.java
+++ src/ZipMain.java
@@ -16,145 +16,159 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class ZipMain {
-	
+
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) throws Exception{
-		
-		// initialize a mongo instance, and handle any exceptions
-		
-		System.out.println("zipcode distance finder calculator test");
-				
-		MongoClient mongoClient = new MongoClient( "localhost" );	
+	public static void main(String[] args) throws Exception {
 
-		DB db = mongoClient.getDB( "ziptest" );
-		
+		// make sure to run mongo first or will throw many exceptions
+
+		System.out.println("zipcode distance finder calculator test");
+
+		MongoClient mongoClient = new MongoClient("localhost");
+
+		DB db = mongoClient.getDB("ziptest");
+
 		DBCollection coll = db.getCollection("zipcodeLocations");
-		
-		
-		DB mass = mongoClient.getDB( "massive" );
-		
-		DBCollection stores = db.getCollection("stores");
-		
-		DBCursor cursor;
-	
+
+		DB mass = mongoClient.getDB("massive");
+
+		DBCollection storesCollection = mass.getCollection("stores");
+
+		System.out.println("storesCollection.getCount() in Main "
+				+ storesCollection.getCount());
+
+		System.out.println("coll.getCount() in Main " + coll.getCount());
+
+		// run this once at the beginning of development session
+		// DataManage.loadZipFileIntoDB();
+
+		// make an arraylist of all the stores
+		DBCursor myCursor;
+		myCursor = storesCollection.find();
+		List<DBObject> stores = new ArrayList<DBObject>();
+		stores = myCursor.toArray();
+		myCursor.close();
+
+		// gets lat and lon for zipcode thisZip
 		Integer zip = 0;
 		Double lat = 0.0;
 		Double lon = 0.0;
-		
-		
-		//load the zipcode lat-lon table into mongodb
-//		File inFile = new File("zipLocs.csv");
-//		if(inFile.isFile()){
-//			coll.drop();
-//			try {			
-//				BufferedReader br = new BufferedReader(new FileReader(inFile));
-//				StringBuilder sb = new StringBuilder();
-//				String line = br.readLine();
-//	
-//				while (line != null) {
-//				//for(int i = 0; i < 1000; i++){
-//					zip = Integer.valueOf(line.substring(0, 5));
-//					lat = Double.valueOf(line.substring(6, 15));
-//					lon = Double.valueOf(line.substring(16, 26).trim());
-//					
-//					BasicDBObject doc = new BasicDBObject("zipcode", zip).
-//			                append("latitude", lat).
-//			                append("longitude", lon);
-//			                
-//					coll.insert(doc);
-//					line = br.readLine();
-//				}
-//
-//				br.close();
-//	
-//			} catch (Exception ex) {
-//				ex.printStackTrace();
-//			}
-//		}
-//		else {
-//			System.out.println("could not find file");
-//		}
-		
-
-//		BasicDBObject doc = new BasicDBObject("zipcode", zip).
-//                append("latitude", lat).
-//                append("longitude", lon);
-//		
-//		coll.insert(doc);
-
-		DBObject myDoc;
-		
-		Integer thisZip = 63026;
-		
-		//myDoc = db.coll.find({ zipcode: thisZip});
-		BasicDBObject query = new BasicDBObject("zipcode", 63025);
-		//BasicDBObject query = new BasicDBObject();
-		BasicDBObject field = new BasicDBObject();
-		field.put("zipcode", 1);
+		DBCursor cursor;
+		Integer thisZip = 90032; // csula zipcode
+		BasicDBObject query = new BasicDBObject("zipcode", thisZip);
 		cursor = coll.find(query);
-		ArrayList result = new ArrayList();
-		
-		//cursor = db.getCollection("zipcodeLocations").find(query,field);
 		try {
-			   while(cursor.hasNext()) {
-				   BasicDBObject obj = (BasicDBObject) cursor.next();
-				   zip = obj.getInt("zipcode");
-				   lat = obj.getDouble("latitude");
-				   lon = obj.getDouble("longitude");
-					//result.add(obj.getString("zipcode"));
-			       //System.out.println(cursor.next());
-			   }
-			} finally {
-			   cursor.close();
+			while (cursor.hasNext()) {
+				BasicDBObject obj = (BasicDBObject) cursor.next();
+				zip = obj.getInt("zipcode");
+				lat = obj.getDouble("latitude");
+				lon = obj.getDouble("longitude");
+				// result.add(obj.getString("zipcode"));
+				// System.out.println(cursor.next());
 			}
-		
-		//myDoc = coll.findOne({zipcode:  });
-		//System.out.println(myDoc);
+		} finally {
+			cursor.close();
+		}
+
 		System.out.println(zip);
 		System.out.println(lat);
 		System.out.println(lon);
-		//System.out.println(result.get(0));
 		System.out.println("");
-		
-		//print out the locations within distance collection
+
+		// build a hashmap of zipcodes that meet the criteria
 		HashMap<Integer, Double> zipDist = new HashMap<Integer, Double>();
 		cursor = coll.find();
+		Double maxDistance = 12.0;
 		try {
-			//for(int i= 0; i < 50; i++){
-			while(cursor.hasNext()) {
+			// for(int i= 0; i < 50; i++){
+			while (cursor.hasNext()) {
 				BasicDBObject obj = (BasicDBObject) cursor.next();
-				Double distance = ZipDistCalc.calcDistance(lat, lon, obj.getDouble("latitude"), obj.getDouble("longitude"));
-			   if(distance  < 10.0){
-				   //System.out.println(cursor.next());
-				   zipDist.put(obj.getInt("zipcode"), distance);		   
-			   }
-		   }
+				Double distance = ZipDistCalc.calcDistance(lat, lon,
+						obj.getDouble("latitude"), obj.getDouble("longitude"));
+				if (distance < maxDistance) {
+					System.out.println("(Integer)obj.getInt(\"zipcode\")   "
+							+ (Integer) obj.getInt("zipcode"));
+					zipDist.put((Integer) obj.getInt("zipcode"), distance);
+				}
+			}
 		} finally {
-		   cursor.close();
+			cursor.close();
 		}
+
+		System.out.println("zipDist hashMap = " + zipDist);
+		System.out.println("zipDist hashMap size = " + zipDist.size());
+
+		HashMap<Double, DBObject> distanceByStore = new HashMap<Double, DBObject>();
+
+		String storeZipS = "";
+		Integer storeZip = 0;
+		Double storeLat = 0.0;
+		Double storeLon = 0.0;
+
+		Iterator<DBObject> iterator = stores.iterator();
+		System.out.println("size of stores list = " + stores.size());
+		int i = 0;
+		DBObject obj;
+		while (iterator.hasNext()) {
+			System.out.println(" i = " + i++);
+			// storeZip =
+			// Integer.getInteger(((iterator.next()).get("zipcode")).toString());
+			obj = iterator.next();
+			storeZipS = (String) (obj).get("zipcode");
+			storeZip = Integer.parseInt(storeZipS);
+			System.out.println("storeZipS var = " + storeZipS);
+			System.out.println("zipDist contains " + storeZip + "  "
+					+ zipDist.containsKey(storeZip));
+			if (zipDist.containsKey(storeZip)) {
+				System.out.println("distance to store = "
+						+ zipDist.get(storeZip));
+				distanceByStore.put(zipDist.get(storeZip), obj);
+			}
+
+			System.out.println(storeZipS);
+			iterator.remove();
+		}
+
+		Map<Double, DBObject> treeMap = new TreeMap<Double, DBObject>(
+				distanceByStore);
+
+		ArrayList<DBObject> returnStores = new ArrayList<DBObject>();
+
+		for (Map.Entry<Double, DBObject> entry : treeMap.entrySet()) {
+			Double key = entry.getKey();
+			System.out.println("\ndistance to store = " + key);
+			DBObject value = entry.getValue();
+			System.out.println(" store ID = " + value.toString());
+			returnStores.add(value);
+		}
+
+		System.out.println("\nreturnStores = " + returnStores);
 		
-		ArrayList storeDist = new ArrayList();
-		
-		System.out.println(zipDist);
-		
-		//DataManage.testForZipsCsv();
-		//DataManage.parseZipsCsv();
-		
-		System.out.println("coll.getCount()" + coll.getCount());
-		
+		// iterate through stores list
+		// check if hashmap zipDist contains that zip as key
+		// if yes add that store object and distance to ArrayList storeDist
+		// order storeDist list by distance small->largest
+		// copy to list containing just the stores in order
+		// return list to client
+
+		// System.out.println(zipDist);
+
 		System.out.println("end of main");
-		
-		
-		//double dist = ZipDistCalc.calcDistance(zip1, zip2);
-		//System.out.println("distance is " + dist);
+
+		// double dist = ZipDistCalc.calcDistance(zip1, zip2);
+		// System.out.println("distance is " + dist);
 	}
-	
-	public static void storeDBcollectionZipcode(){
-		
+
+	public static void storeDBcollectionZipcode() {
+
 	}
 
 }
